d3abd454fa7352bfd772aecc471308ec277495b2
Jonas Valereo <jvalereo2016@gmail.com>
2021-04-13 00:02:04 -0300

    Initial commit

---
+++ src/aula11/Aula11.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula11;
+
+/**
+ *
+ * @author JONAS
+ */
+public class Aula11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Treinamento - Aula11, inserir dados, tipo int, double*/
+        //Jonas Valereo - Técnico em Informática
+        
+        //Declarando a variavel tipo int, double
+        
+        int a = 10;
+        int b = 20;
+        double resultado;
+        
+        
+        //Declarando a variavel resultado
+        
+        resultado = a + b;
+        
+    
+        //Imprimir, saida de dados, Console System
+        
+        System.out.println("Resultado = " + resultado);
+        
+      
+        //Saida da aplicação e descarregar a mamoria
+        
+        System.exit(0);
+        
+        //Fim do programa
+    }
+    
+}
